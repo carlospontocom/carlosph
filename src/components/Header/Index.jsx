6041b34839ea243d3e0391e973607f85462e9826
frontendcnn <frontendcnn@gmail.com>
2025-11-07 22:35:34 +0000

    Formatando header

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,26 +1,31 @@
 import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
         <header className={styles.Header}>
-            <div className={styles.navbar}>
-                <div className={styles.logo}>
-                    <Link to="/">Logo</Link>
+            <div className='container'>
+                <div className={styles.navbar}>
+                    <div>
+                        <NavLink to="/" className={styles.logo}>
+                            <i class="fa-brands fa-creative-commons-remix"></i>
+                            <span>React4pp</span>
+                        </NavLink>
+                    </div>
+                    <nav className={styles.menu}>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about">Sobre</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/formulario">Cadastre-se</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul className={styles.navList}>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">Sobre</Link>
-                        </li>
-                        <li>
-                            <Link to="/formulario">Cadastre-se</Link>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </header>
     )
